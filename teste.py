1d01ec89adb4a75571b5604c7b7a44193cf6b7f9
Dennis JB Rocha <58828866+dennis310797@users.noreply.github.com>
2021-03-17 13:15:13 -0300

    Update teste.py
    Valor mínimo de cada salário

--- teste.py
+++ teste.py
@@ -20,7 +20,8 @@ with open('funcionario.json', 'r') as json_file:
     group=df_full[['nome_completo','salario']]
     salario=df_full.salario
     temp=0
-    df={}
+    df={} #def_max={}
+    #df_min={}
 
     #4- FUNÇÃO PARA CONFERIR E SALVAR O MAIOR SALARIO E O NOME AND SALVAR SALARIOS IGUAIS
     for i in range(len(df_full)):
@@ -29,6 +30,15 @@ with open('funcionario.json', 'r') as json_file:
                 temp=salario[i]
                 df_list=list(group.iloc[i])
                 df[df_list[0]]=df_list[1] #GERA UMA DICT COM NOME:SALARIO
+        """
+No final da execução do for terá um temp com o valor máximo
+     for rows in range(len(df_full)):
+        if temp>=salário[rows]:
+             if temp != salario[i] or temp == salario[i]:
+             temp=salario[rows]
+             df_list=list(group.iloc[rows])
+             df_min[df_list[0]]=df_list[1]
+        """
 
     """
     pega o salario maximo
